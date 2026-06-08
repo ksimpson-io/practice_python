f174f4ef54542efcabfeab1055940d1d3af582b6
4rudy <109769396+4rudy@users.noreply.github.com>
2026-06-08 17:27:54 -0500

    alt string reversing

--- 6_string_lists/main.py
+++ 6_string_lists/main.py
@@ -3,7 +3,8 @@
 
 def start():
     word = input("Enter a word: ")
-    backwards = ("".join(reversed(word)))
+    # backwards = "".join(reversed(word))
+    backwards = word[::-1]
 
     pal = "is" if (word == backwards) else "is NOT"
     print(f'{word} {pal} a palindrome')
